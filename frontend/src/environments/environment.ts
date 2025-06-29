8644f08c888ea2a8a343ad3601574ce2746cf1be
Ghioca Iuliana <165220384+iuligh@users.noreply.github.com>
2025-06-29 21:44:49 +0300

    Delete frontend/src/environments/environment.ts

--- frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api',
-  stripePublicKey: 'pk_test_51Rb6YXH8eUv0d48q82CotuBelIVldSRbFQ3ZdGFbp4ppAgkVriZUAZ2quQVNxn7BYJFjWygD2GBgedQkaFzBFyU000ZiXO5ZVH'
-};
